3cee3102fb963fb0b46dd98ef78269379893ad7d
mcfancy123 <simeonmensah0@gmail.com>
2025-02-08 12:41:25 +0000

    Add files via upload
    initial commit

---
+++ project1.py
@@ -0,0 +1,18 @@
+#Personal info
+name="Mensah Simeon"
+print(name)
+
+address="GW-0020-4484"
+print(address)
+
+city="Kuamsi"
+print(city)
+
+region="Ashanti Region"
+print(region)
+
+zip="0000"
+print(zip)
+
+phone_number=int(+233_0538231904)
+print(phone_number)
